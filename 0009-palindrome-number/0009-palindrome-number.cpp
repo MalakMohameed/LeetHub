f1410a42ca3d0e0a8cd5d897b17a5dc74233883d
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2026-01-23 00:12:27 +0200

    Time: 7 ms (15.55%), Space: 11.9 MB (5.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-       if (x < 0) {
-        return false;
-    }
-    
-    string s = to_string(x);
-    int n = s.size();
-    
-    for (int i = 0; i < n / 2; ++i) {
-        if (s[i] != s[n - 1 - i]) {
-            return false;
-        }
-    }
-    
-    return true; 
+        string original = to_string(x);
+        string reverse(original.rbegin(), original.rend());
+        return original == reverse;
     }
 };
