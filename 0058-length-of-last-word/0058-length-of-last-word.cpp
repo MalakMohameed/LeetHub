7acbfd2161a6cedd9f7e05eefd7471d51b4e2865
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2026-01-24 21:19:57 +0200

    Time: 0 ms (100%), Space: 8.9 MB (67.22%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int counter = 0;
+        for(int i = s.length()-1; i >=0; i--){
+            if(s[i] == ' '){
+                if(counter > 0) return counter;
+            }
+            else counter++;
+        }
+        return counter;
+    }
+};
