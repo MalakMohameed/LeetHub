e7c767051ad45d504aaa638f0654433cea0666e1
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2026-07-30 11:46:16 +0300

    Time: 530 ms (12.6%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,5 +1,3 @@
-select author_id as id 
-from Views 
-where author_id  = viewer_id
-group by id
-order by id asc;
+select distinct author_id as id from Views
+ where author_id = viewer_id 
+ order by id ASC;
