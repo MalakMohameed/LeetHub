00a98907731810f6be939a02f1568c785da49f62
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2026-08-03 15:39:34 +0300

    Time: 401 ms (36.01%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,12 @@
+SELECT query_name, 
+    ROUND(AVG(rating / position), 2) AS quality, 
+    ROUND(
+    SUM(IF(rating < 3, 1, 0)) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries 
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
