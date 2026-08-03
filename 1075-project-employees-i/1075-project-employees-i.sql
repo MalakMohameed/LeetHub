54b81a0547986812a77f53fddf6c731e3f5a1bd9
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2026-08-03 14:57:11 +0300

    Time: 594 ms (27.17%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,9 @@
+SELECT p.project_id, ROUND(AVG(e.experience_years), 2) AS average_years 
+FROM project AS p
+LEFT JOIN employee AS e
+ON p.employee_id = e.employee_id 
+GROUP BY p.project_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
